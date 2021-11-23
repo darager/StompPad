a22086e9d05c76673dc377a13f61fec72e2bbbad
Rager Florian <florianrager90@gmail.com>
2021-11-23 15:05:59 +0100

    Enable usb keyboard mode (does not exist on esp8266) have to buy other uC

--- firmware/main.py
+++ firmware/main.py
@@ -1,5 +1,13 @@
 import time
 from machine import Pin
+import pyb
+
+kb = pyb.USB_HID()
+# pyb.delay(5)
+# # Do the key up
+# buf[0] = 0x00
+# buf[2] = 0x00
+# kb.send(buf)
 
 pin = Pin(5, Pin.IN, Pin.PULL_UP)
 
@@ -7,3 +15,11 @@ while True:
     time.sleep(1)
     value = pin.value() # if value is 0 button is pressed
     print(time.ticks_ms(), value)
+
+    if value == 0:
+        buf = bytearray(8)
+        # Sending T
+        # Do the key down
+        buf[0] = 0x02 # LEFT_SHIFT
+        buf[2] = 0x17 # keycode for 't/T'
+        kb.send(buf)
