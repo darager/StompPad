6515c41a53aaaff5ef25c8144d73101bb1f25097
Rager Florian <florianrager90@gmail.com>
2021-11-28 11:14:53 +0100

    Add keyboard Lib

--- firmware/boot.py
+++ firmware/boot.py
@@ -1,6 +1,5 @@
-#boot.py
 import machine
 import pyb
 
-#pyb.main('main.py') # main script to run after this one
-pyb.usb_mode('CDC+HID',hid=pyb.hid_keyboard)
+pyb.main('main.py') # main script to run after this one
+# pyb.usb_mode('CDC+HID',hid=pyb.hid_keyboard)
--- firmware/main.py
+++ firmware/main.py
@@ -1,25 +1,17 @@
 import time
+import keyboard
 from machine import Pin
-import pyb
 
-kb = pyb.USB_HID()
-# pyb.delay(5)
-# # Do the key up
-# buf[0] = 0x00
-# buf[2] = 0x00
-# kb.send(buf)
+led = Pin(25, Pin.OUT)
+button = Pin(16,  Pin.IN, Pin.PULL_UP)
 
-pin = Pin(5, Pin.IN, Pin.PULL_UP)
+k = keyboard.Keyboard()
+a = 0x04;
 
 while True:
-    time.sleep(1)
-    value = pin.value() # if value is 0 button is pressed
-    print(time.ticks_ms(), value)
+    time.sleep(0.1)
+    led.value(button.value())
 
-    if value == 0:
-        buf = bytearray(8)
-        # Sending T
-        # Do the key down
-        buf[0] = 0x02 # LEFT_SHIFT
-        buf[2] = 0x17 # keycode for 't/T'
-        kb.send(buf)
+    if not button.value():
+        k.press(a)
+        k.release();
