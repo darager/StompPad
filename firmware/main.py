97d10a0867ab2206e9f8bf0bc055aa24061839ad
Rager Florian <florianrager90@gmail.com>
2021-11-29 10:43:35 +0100

    Extract handling of pin and keyboard

--- firmware/main.py
+++ firmware/main.py
@@ -2,23 +2,32 @@ import time
 import keyboard
 from machine import Pin
 
-led = Pin(25, Pin.OUT)
-button = Pin(16,  Pin.IN, Pin.PULL_UP)
+class Switch:
+    def __init__(self, pinId, keys, keyboard):
+        self.keyboard = keyboard
+        self.keys = keys
+
+        self.pin = Pin(pinId, Pin.IN, Pin.PULL_UP)
+        self.lastPinState = not self.pin.value()
+
+    def checkForInputValueChange(self):
+        currState = not self.pin.value()
+
+        if self.lastPinState == False and currState == True:
+            for key in self.keys:
+                self.keyboard.press(key)
+            for key in self.keys:
+                self.keyboard.release(key)
+
+        self.lastPinState = currState
 
 k = keyboard.Keyboard()
-a = 0x04;
-
-prev = False
+switches = [
+    Switch(16, [0x05], k)
+]
 
 while True:
+    for switch in switches:
+        switch.checkForInputValueChange()
+
     time.sleep(0.05)
-
-    curr = not button.value()
-
-    led.value(curr)
-
-    if prev == False and curr == True:
-        k.press(a)
-        k.release_all()
-
-    prev = curr
