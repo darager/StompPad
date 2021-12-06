1bbb6f22b00f08a20ea66ff933a37950650a244a
Rager Florian <florianrager90@gmail.com>
2021-12-06 21:05:07 +0100

    Rework using interrupts instead of checking in loop

--- firmware/main.py
+++ firmware/main.py
@@ -3,28 +3,43 @@ import keyboard
 from keyboard import Key
 from machine import Pin
 
-k = keyboard.Keyboard()
 
-class Switch:
-    def __init__(self, pinId, onPress):
-        self.pin = Pin(pinId, Pin.IN, Pin.PULL_UP)
-        self.lastPinState = not self.pin.value()
-        self.onPress = onPress
+class DebouncedSwitch:
+    def __init__(self, pin, pinOnState = 0, debounceTime = 5):
+        self.pin = pin
+        self.pinOnState = pinOnState
+        self.debounceTime = debounceTime
+        self.lastState = self.pin.value()
+        self.OnPressed = []
+        self.OnReleased = []
+        pin.irq(self.handleInterrupt, Pin.IRQ_RISING | Pin.IRQ_FALLING)
 
-    def checkForInputValueChange(self):
-        currState = not self.pin.value()
+    def _wait_pin_change(self):
+        # wait for pin to change value
+        # it needs to be stable for the debounceTime
+        sTime = self.debounceTime / 1000
+        cur_value = self.pin.value()
+        active = 0
+        while active < 20:
+            if self.pin.value() != cur_value:
+                active += 1
+            else:
+                active = 0
+            time.sleep(sTime)
 
-        if self.lastPinState == False and currState == True:
-            if self.onPress:
-                self.onPress()
+    def handleInterrupt(self, pinInfo):
+        self._wait_pin_change()
+        currentState = self.pin.value()
 
-        self.lastPinState = currState
+        if self.lastState is not self.pinOnState and currentState is self.pinOnState:
+            for f in self.OnPressed:
+                f()
+        else:
+            for f in self.OnReleased:
+                f()
 
-def createSendFunc(keys, keeb):
-    def send():
-        keeb.press(*keys)
-        keeb.release(*keys)
-    return send
+        self.lastState = currentState
+
 
 alt = Key.MOD_LEFT_ALT
 alt_i = [alt, Key.i]
@@ -33,25 +48,42 @@ alt_k = [alt, Key.k]
 alt_l = [alt, Key.l]
 alt_u = [alt, Key.u]
 
-switchPins = [ 17, 16,
-             20, 19, 18]
-layers = {
-    "default": [ alt_i, alt_u,
-              alt_j, alt_k, alt_l ]
+keeb = keyboard.Keyboard()
+currentLayer = "default"
+
+def layer(layerName):
+    def swapLayer():
+        currentLayer = layerName
+    return swapLayer
+
+switchPins = [ 17, 16, 20, 19, 18]
+
+keybindings = {
+    "default":      [ alt_i, alt_u, alt_j, alt_k, layer("default_hold") ],
+    "default_hold": [ Key.I, Key.U, Key.J, Key.K, layer("default") ]
 }
 
-switches = []
-for i in range(len(switchPins)):
-    pin = switchPins[i]
+def handleBinding(keybinding):
+    if keybinding is None:
+        return
 
-    keys = layers["default"][i]
-    send = createSendFunc(keys, k)
+    print(keybinding)
+    # currently have no way of telling if typeof Function
+    # try:
+    #     keybinding()
+    # except:
+    #     keys = keybinding
+    #     keeb.press(*keys)
+    #     keeb.release(*keys)
 
-    switch = Switch(pin, send)
-    switches.append(switch)
+def getBinding(idx, layerName):
+    return keybindings[layerName][idx]
 
-while True:
-    for switch in switches:
-        switch.checkForInputValueChange()
 
-    time.sleep(0.05)
+def onpress():
+    binding = getBinding(0, "default")
+    handleBinding(binding)
+
+pin = Pin(17, Pin.IN, Pin.PULL_UP)
+debSwitch = DebouncedSwitch(pin);
+debSwitch.OnPressed.append(onpress)
